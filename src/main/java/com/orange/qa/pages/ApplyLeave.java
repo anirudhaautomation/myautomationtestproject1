7c9cfd076fdc0ac38e7ef8a7bd46fef03a8fa5d6
Anirudha <anirudha.1986@gmail.com>
2021-04-15 11:40:49 +0530

    added ApplyLeave.java file

---
+++ src/main/java/com/orange/qa/pages/ApplyLeave.java
@@ -0,0 +1,11 @@
+package com.orange.qa.pages;
+
+import org.openqa.selenium.support.PageFactory;
+
+import com.orange.qa.base.TestBase;
+
+public class ApplyLeave extends TestBase{
+public ApplyLeave() {
+	PageFactory.initElements(driver, this);
+}
+}
